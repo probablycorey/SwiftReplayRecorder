7abc8188e3b0253a3c3f06bc54a968229ba9b698
Corey Johnson <probablycorey@gmail.com>
2021-06-30 11:12:07 -0700

    Initial Commit

---
+++ SwiftReplayRecorder/SwiftReplayRecorderApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftReplayRecorderApp.swift
+//  SwiftReplayRecorder
+//
+//  Created by Corey Johnson on 6/30/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftReplayRecorderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
